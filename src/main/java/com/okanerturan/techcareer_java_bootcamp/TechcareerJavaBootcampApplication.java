69448232101dfc4f5805ead6b4122ba4c8b77db6
okturan <okanert@gmail.com>
2024-12-30 11:34:37 +0300

    Initialize Spring Boot project with Maven wrapper
    This commit sets up a basic Spring Boot project with Maven wrapper for consistent builds across environments. It includes configuration files, dependencies (e.g., Spring, H2, Lombok), and initial project structure with a main application class and a test class.

--- src/main/java/com/okanerturan/techcareer_java_bootcamp/TechcareerJavaBootcampApplication.java
+++ src/main/java/com/okanerturan/techcareer_java_bootcamp/TechcareerJavaBootcampApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TechcareerJavaBootcampApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TechcareerJavaBootcampApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TechcareerJavaBootcampApplication.class, args);
+    }
 
 }
